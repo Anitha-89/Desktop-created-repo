fbea95ae75aa9db0f4fca1d7ba3cdc34b9c0ceb6
Anitha-89 <anithabalarathnam48@gmail.com>
2025-01-25 07:01:04 +0530

    Create local_script.py
    Updated_local_script

---
+++ local_script.py
@@ -0,0 +1,12 @@
+import random
+
+quotes = 
+[
+    "Stay positive!",
+    "You can do it!",
+    "Dream big!",
+    "Believe in you!"
+]
+
+print("A motivational quote for you:")
+print(random.choice(quotes))
